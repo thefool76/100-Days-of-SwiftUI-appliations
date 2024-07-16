9e638f6ea3d23b60139ecfd7fb829ab03fbd8647
thefool76 <sumitmishra1135@gmail.com>
2024-07-16 18:45:12 +0530

    Initial commit of Xcode project

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Bhavesh Mishra on 15/07/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
