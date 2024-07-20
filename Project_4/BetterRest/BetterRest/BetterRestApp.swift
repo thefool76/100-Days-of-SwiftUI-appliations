518fff0311a06d7ce80b8bdd82cdf44d095c6ebf
thefool76 <sumitmishra1135@gmail.com>
2024-07-20 17:30:32 +0530

    added BetterRest App

---
+++ Project_4/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Bhavesh Mishra on 20/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
