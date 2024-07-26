70f6dbecb9e9fae05bff99e829dc930f47f3569c
thefool76 <sumitmishra1135@gmail.com>
2024-07-26 18:19:25 +0530

    done with the animation

---
+++ Project_6/Animation/Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Bhavesh Mishra on 26/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project_6/Animation/Animation/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  Animation
+//
+//  Created by Bhavesh Mishra on 26/07/24.
+//
+
+import SwiftUI
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading), identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+struct ContentView: View {
+    @State private var isShowingRed = true
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+            }
+        }
+        .onTapGesture {
+            withAnimation{
+                isShowingRed.toggle()
+                    
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
