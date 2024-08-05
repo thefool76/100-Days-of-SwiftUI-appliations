9d2ec1c327919dd017a95864168251d56c15e166
thefool76 <sumitmishra1135@gmail.com>
2024-08-05 19:15:32 +0530

    Connected google firestore done

--- Dev/spotify_clone/lib/data/sources/auth/auth_firebase_service.dart
+++ Dev/spotify_clone/lib/data/sources/auth/auth_firebase_service.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:spotify_clone/data/models/auth/create_user.dart';
@@ -31,8 +32,15 @@ class AuthFirebaseServiceImpl extends AuthFirebaseService {
   @override
   Future<Either> signup(CreateUser createUser) async {
     try {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      var data = await FirebaseAuth.instance.createUserWithEmailAndPassword(
           email: createUser.email, password: createUser.password);
+
+      FirebaseFirestore.instance.collection('Users').add(
+        {
+          'name' : createUser.fullName,
+          'email' : data.user?.email
+        }
+      );
       return const Right("Signup was success");
     } on FirebaseAuthException catch (e) {
       String message = '';
